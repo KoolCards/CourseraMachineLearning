39beaaf3722eca54d1e975717ca11b36bb0318d3
KoolCards <krissatya09@gmail.com>
2016-11-25 13:37:36 -0800

    update backprop

--- HandwrittenBackPropagation/ex4/nnCostFunction.m
+++ HandwrittenBackPropagation/ex4/nnCostFunction.m
@@ -75,12 +75,27 @@ endfor
 leftvalues = -yvec'.*log(predictions);
 rightvalues = (1-yvec').*log(1-predictions);
 J = (1/m)*sum(sum(leftvalues - rightvalues)) + (lambda/(2*m))*((sum(sum(Theta1(:,2:end).^2)))+(sum(sum(Theta2(:,2:end).^2))));
-% -------------------------------------------------------------
-
-% =========================================================================
-
-% Unroll gradients
+for t=1:m
+  a1 = [1; X(t,:)'];
+  z2 = Theta1*a1;
+  a2 = [1; sigmoid(z2)];
+  z3 = Theta2*a2;
+  a3 = sigmoid(z3);
+  for i=1:num_labels
+    if (i == y(t,1))
+      yy(i,1) = 1;
+    else
+      yy(i,1) = 0;
+    endif
+  endfor
+  delta_3 = a3 - yy;
+  delta_2 = Theta2'*delta_3.*[1; sigmoidGradient(z2)];
+  delta_2 = delta_2(2:end);
+  Theta1_grad = Theta1_grad + delta_2 * a1';
+	Theta2_grad = Theta2_grad + delta_3 * a2';
+endfor
+Theta1_grad = (1/m) * Theta1_grad + (lambda/m)*[zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
+Theta2_grad = (1/m) * Theta2_grad + (lambda/m)*[zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
-
-
 end
+
--- HandwrittenBackPropagation/ex4/randInitializeWeights.m
+++ HandwrittenBackPropagation/ex4/randInitializeWeights.m
@@ -5,7 +5,7 @@ function W = randInitializeWeights(L_in, L_out)
 %   of a layer with L_in incoming connections and L_out outgoing 
 %   connections. 
 %
-%   Note that W should be set to a matrix of size(L_out, 1 + L_in) as
+%   Note that W should be set t/o a matrix of size(L_out, 1 + L_in) as
 %   the column row of W handles the "bias" terms
 %
 
@@ -18,13 +18,8 @@ W = zeros(L_out, 1 + L_in);
 %
 % Note: The first row of W corresponds to the parameters for the bias units
 %
-
-
-
-
-
-
-
+epsilon_init = 0.12;
+W = rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init;
 
 
 % =========================================================================
